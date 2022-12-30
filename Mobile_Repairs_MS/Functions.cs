f6cdb723a7eced9a22caf63268ba90ddb02eab85
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-30 16:48:09 +0200

    Abdulrahman Khalifa ConnectionFunc to Connect With DataBase

--- Mobile_Repairs_MS/Functions.cs
+++ Mobile_Repairs_MS/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,37 @@ namespace Mobile_Repairs_MS
 {
     class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private String ConStr;
+
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\defds\Documents\MobileRepairDB(AbdulrahmanKhalifa).mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+        public DataTable GetData(String Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(String Query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            return cnt;
+        }
     }
 }
